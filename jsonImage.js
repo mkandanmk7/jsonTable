856c6b40cbecefa01071dfaa5aa9e4f150228e94
Muthu <84056393+mkandanmk7@users.noreply.github.com>
2021-07-23 10:39:13 +0530

    Create jsonImage.js

---
+++ jsonImage.js
@@ -0,0 +1,57 @@
+
+    let technical={
+        "dimensions":{
+            "Overall length":3500,
+            "Overall width":1600,
+            "Overall height":1490,
+            "Wheelbase":2360,
+            "Track width":{
+                "Front":1405,
+                "Rear":1400
+            },
+            "Minimum turning radius":4.5,
+            "Minimum ground clearance":170
+        },
+        "Capacities":{
+            "seating capacity":5,
+            "Fuel tank capacity":35
+
+        },
+        "Engine":
+        {
+            "Type":"KB-Series",
+            "Number of cylinders":3,
+            "Number of Valves":12,
+            "Capacity":998,
+            "Bore x stroke":"73.0 x 79.5",
+            "Compression ratio":10:1,
+            "Maximum power":"61/5.200",
+            "Maximum torque":90/3.500,
+            "Fuel distribution":"Multipoint Injection"
+
+        },
+        Transmission:
+        {
+            "Type":"5-speed MT"
+        },
+        chassis:{
+            "Steering":["rack","pinion","power assisted"],
+            "Breaks":{
+                "front":"ventilated disks",
+                "rear":"Drums"
+    
+            },
+            "suspension":{
+                "front":"MacPherson struct & coil spring",
+                "rear":"Isolated trailing link & coil spring"
+            },
+            "shock absorbs":"gas filled",
+            "Tyre(tubeless)":155/80R13,
+            "weights":{
+                "keb weight(min)":860-880,
+                "gross vehicle weight":1320
+            }
+        }
+
+
+    }
